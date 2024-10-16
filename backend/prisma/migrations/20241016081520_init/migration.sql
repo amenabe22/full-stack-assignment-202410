928dd65818023950721b845f4403548f5f4ad086
amenabe22 <bdere12345@gmail.com>
2024-10-16 11:17:40 +0300

    Added Dockerfile and docker compose

--- backend/prisma/migrations/20241016081520_init/migration.sql
+++ backend/prisma/migrations/20241016081520_init/migration.sql
@@ -2,5 +2,7 @@
 CREATE TABLE "Conversation" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "messages" TEXT NOT NULL,
+    "convId" TEXT,
+    "isAI" BOOLEAN NOT NULL DEFAULT false,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
